f6af3b869e3e6abf131a3b47401e18052c15f0c4
Ali Mustafa <112906917+AliMstf03@users.noreply.github.com>
2025-09-25 14:44:20 +0200

    Update DevopsTrainingApplication.java

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-public final class DevopsTrainingApplication {
+public class DevopsTrainingApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
 
+    protected DevopsTrainingApplication() {
+        // Protected constructor to satisfy Checkstyle while allowing Spring to instantiate
+    }
 }
