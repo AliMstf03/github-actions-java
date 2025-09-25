e678ba2c109d178230d4e800122008c47e40c10e
Ali Mustafa <112906917+AliMstf03@users.noreply.github.com>
2025-09-25 14:42:04 +0200

    Error 1.1

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public final class DevopsTrainingApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
-    @SuppressWarnings("unused")
-    private final String appName = "DevopsTrainingApplication";
 
-    
 }
