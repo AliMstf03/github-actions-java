b9209fc5818be25c57e5b5694d51bd2292d06ba6
Ali Mustafa <112906917+AliMstf03@users.noreply.github.com>
2025-09-25 14:24:55 +0200

    Update DevopsTrainingApplication.java

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsTrainingApplication {
 
     private DevopsTrainingApplication() {
-        // prevent instantiation
+        //Error 1
     }
 
     public static void main(String[] args) {
